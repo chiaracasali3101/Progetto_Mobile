d6acbb93d767c5c09a9b8d15cf73e09010a9d98e
Lolalolytaaaa <laura.scotti3@studio.unibo.it>
2026-05-15 11:25:48 +0200

    Ottimizzazione della struttura di dominio del progetto

--- data/src/main/java/com/unibo/android/data/di/RepositoryProviderImpl.kt
+++ data/src/main/java/com/unibo/android/data/di/RepositoryProviderImpl.kt
@@ -1,7 +1,6 @@
 package com.unibo.android.data.di
 
 import com.unibo.android.data.repositories.AccommodationRepositoryImpl
-import com.unibo.android.domain.di.RepositoryProvider
 import com.unibo.android.domain.repositories.AccommodationRepository
 
 class RepositoryProviderImpl: RepositoryProvider {
--- domain/src/main/java/com/unibo/android/domain/di/RepositoryProvider.kt
+++
@@ -1,7 +0,0 @@
-package com.unibo.android.domain.di
-
-import com.unibo.android.domain.repositories.AccommodationRepository
-
-interface RepositoryProvider {
-    val accommodationRepository: AccommodationRepository
-}
