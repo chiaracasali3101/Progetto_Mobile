2d1d88640cc32fa72e0dade4682ddf8f740a02f2
anadiarciuc001 <anadiarciuc18@gmail.com>
2026-05-15 13:51:55 +0200

    sistemate dipendenze nel build gradle ui

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
+    // 1. IL NUOVO PLUGIN PER KOTLIN 2.0 (ORA CON LA VERSIONE CORRETTA!)
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
 }
 
 android {
@@ -12,7 +14,11 @@ android {
 
     buildFeatures {
         viewBinding = true
+        // ECCO LA MAGIA: Diciamo ad Android Studio di accendere Jetpack Compose!
+        compose = true
     }
+
+    // (Il blocco composeOptions che c'era prima è stato eliminato, come richiede Kotlin 2.0!)
 
     defaultConfig {
         minSdk = 31
@@ -47,11 +53,21 @@ dependencies {
     implementation(libs.fragment.ktx)
     implementation(libs.androidx.recyclerview)
 
-    //Versione vecchia
-    //implementation("com.github.bumptech.glide:glide:5.0.5")
-    //implementation ("androidx.cardview:cardview:1.0.0")
+    // Versione vecchia
+    // implementation("com.github.bumptech.glide:glide:5.0.5")
+    // implementation ("androidx.cardview:cardview:1.0.0")
     implementation (libs.androidx.cardview)
     implementation(libs.glide)
+
+    // --- I "DIZIONARI" DI COMPOSE ---
+    val composeBom = platform("androidx.compose:compose-bom:2024.04.01")
+    implementation(composeBom)
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material:material-icons-extended")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    // ------------------------------------
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
