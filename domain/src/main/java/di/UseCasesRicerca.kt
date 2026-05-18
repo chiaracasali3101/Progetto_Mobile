b20fe49c2562f929e6730840088e0974a71c8a59
Chiara <chiaracasali3101@gmail.com>
2026-05-18 12:56:20 +0200

    UseCases

---
+++ domain/src/main/java/di/UseCasesRicerca.kt
@@ -0,0 +1,15 @@
+package com.unibo.android.domain.di
+
+import com.unibo.android.data.entities.FilmEntity
+import com.unibo.android.data.repository.FilmRepository
+
+class UseCasesRicerca(
+    private val repository: FilmRepository
+) {
+
+
+    suspend operator fun invoke(id: Int): List<FilmEntity> {
+        return repository.getFilmsByQuery(id)
+    }
+
+}
